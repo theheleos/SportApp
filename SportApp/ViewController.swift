90bdebfa9d9aaf453d7b000b53f0e9aba800487a
Leonid <78638629+theheleo@users.noreply.github.com>
2023-02-24 22:50:39 +0300

    initial commit

---
+++ SportApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportApp
+//
+//  Created by Леонид  Егоров on 24.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
